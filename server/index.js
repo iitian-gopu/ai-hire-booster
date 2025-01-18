4fc846b55c091e41a48355c15599afd667e4cff2
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-01-18 12:11:22 +0530

    Extend index (server)

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
+const app = express()
+app.use(cors({
+    origin:"https://ai-hire-booster.onrender.com",
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth" , authRouter)
