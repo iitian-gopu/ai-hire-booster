9b9e57001fb4146dc4502d51c6f639795c162c94
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-01-18 13:38:24 +0530

    Finalize index (server)

--- server/index.js
+++ server/index.js
@@ -19,3 +19,13 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth" , authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/interview" , interviewRouter)
+app.use("/api/payment" , paymentRouter)
+
+const PORT = process.env.PORT || 6000
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`)
+    connectDb()
+})
+
