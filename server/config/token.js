520eff8ea13c8f06c086991536d38e09a9df4603
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-01-25 23:10:17 +0530

    Finalize token (server)

--- server/config/token.js
+++ server/config/token.js
@@ -5,3 +5,9 @@ const genToken = async (userId) => {
         const token = jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
 return token
     } catch (error) {
+        console.log(error)
+    }
+
+}
+
+export default genToken
