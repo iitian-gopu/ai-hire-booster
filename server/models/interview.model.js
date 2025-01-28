1540f4a9ef110cc0871726da5db3c15e5ac2b3cc
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-01-28 17:43:16 +0530

    Finish interview.model (server)

--- server/models/interview.model.js
+++ server/models/interview.model.js
@@ -40,3 +40,13 @@ const interviewSchema = new mongoose.Schema({
     finalScore: { type: Number, default: 0 },
 
     status: {
+      type: String,
+      enum: ["Incompleted", "completed"],
+      default: "Incompleted",
+    }
+},{timestamps:true})
+
+const Interview = mongoose.model("Interview" , interviewSchema)
+
+
+export default Interview
