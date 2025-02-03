507c06583545a24b643cead5d632c6168f98cb8a
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-02-03 16:27:57 +0530

    Flesh out isAuth (server)

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -7,3 +7,12 @@ const isAuth = async (req,res,next) => {
 
         if(!token){
             return res.status(400).json({message:"user does not have a token"})
+        }
+        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
+        
+        if(!verifyToken){
+            return res.status(400).json({message:"user does not have a valid token"})
+        }
+        req.userId = verifyToken.userId
+
+        next()
