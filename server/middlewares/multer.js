f2624ce49f5f090ca30e656f281ad7079b9fda59
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-02-04 12:54:59 +0530

    Set up multer (server)

---
+++ server/middlewares/multer.js
@@ -0,0 +1,9 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req, file , cb){
+        cb(null , "public")
+    },
+    filename: function(req , file , cb){
+        const filename = Date.now() + "-" + file.originalname;
+        cb(null , filename)
