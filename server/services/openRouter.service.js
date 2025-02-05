9e4f47814e281e5110468b295d7b7da5de9389b1
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-02-05 16:09:13 +0530

    Create openRouter.service (server)

---
+++ server/services/openRouter.service.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const askAi = async (messages) => {
+    try {
+        if(!messages || !Array.isArray(messages) || messages.length === 0) {
+            throw new Error("Messages array is empty.");
+        }
+        const response = await axios.post("https://openrouter.ai/api/v1/chat/completions",
+            {
+                model: "openai/gpt-4o-mini",
+                messages: messages
