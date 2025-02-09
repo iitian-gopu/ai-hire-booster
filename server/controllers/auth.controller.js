78c2125759cca9d8725f2dd4729f6f1f05183c4c
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-02-09 23:34:38 +0530

    Improve auth.controller (server)

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -9,3 +9,13 @@ export const googleAuth = async (req,res) => {
         if(!user){
             user = await User.create({
                 name , 
+                email
+            })
+        }
+        let token = await genToken(user._id)
+        res.cookie("token" , token , {
+            http:true,
+            secure:true,
+            sameSite:"none",
+            maxAge:7 * 24 * 60 * 60 * 1000
+        })
