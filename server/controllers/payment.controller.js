3bc77a2f162d322f9ae7118be4adea2dc4c61b32
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-02-21 21:10:08 +0530

    Extend payment.controller (server)

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -42,3 +42,14 @@ export const verifyPayment = async (req,res) => {
       razorpay_payment_id,
       razorpay_signature} = req.body
 
+      const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({ message: "Invalid payment signature" });
+    }
+
