52c95b1c0c09ad71f5cf9657397e5c5053734cd0
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-03-02 16:17:22 +0530

    Finish interview.route (server)

--- server/routes/interview.route.js
+++ server/routes/interview.route.js
@@ -9,3 +9,13 @@ import { analyzeResume, finishInterview, generateQuestion, getInterviewReport, g
 const interviewRouter = express.Router()
 
 interviewRouter.post("/resume",isAuth,upload.single("resume"),analyzeResume)
+interviewRouter.post("/generate-questions",isAuth,generateQuestion)
+interviewRouter.post("/submit-answer",isAuth,submitAnswer)
+interviewRouter.post("/finish",isAuth,finishInterview)
+
+interviewRouter.get("/get-interview",isAuth,getMyInterviews)
+interviewRouter.get("/report/:id",isAuth,getInterviewReport)
+
+
+
+export default interviewRouter
