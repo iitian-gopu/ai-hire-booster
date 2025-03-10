f4f1552f7c48781e2e9f55ec8582180d40706e35
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-03-10 12:56:36 +0530

    Refine eslint.config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -8,3 +8,13 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
