079bd3f3e3eb3d25a08e0742f1a41d668d196333
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-04-01 10:24:20 +0530

    Scaffold firebase

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,10 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "aiinterviewer-98add.firebaseapp.com",
+  projectId: "aiinterviewer-98add",
+  storageBucket: "aiinterviewer-98add.firebasestorage.app",
+  messagingSenderId: "573040301522",
+  appId: "1:573040301522:web:dec83e8ce6fc552c28a40b"
