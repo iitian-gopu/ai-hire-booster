4ef4448e07a50f758a514b7770d31c96e5d3ff03
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-04-11 11:02:31 +0530

    Scaffold AuthModel component

---
+++ client/src/components/AuthModel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { FaTimes } from "react-icons/fa";
+import Auth from '../pages/Auth';
+
+function AuthModel({onClose}) {
+    const {userData} = useSelector((state)=>state.user)
+
+    useEffect(()=>{
+        if(userData){
