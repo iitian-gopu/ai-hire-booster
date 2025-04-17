435bead36befe79350f75cdc6a604484d57fa243
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-04-17 18:01:21 +0530

    Update Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,3 +31,14 @@ function Navbar() {
         }
     }
   return (
+    <div className='bg-[#f3f3f3] flex justify-center px-4 pt-6'>
+        <motion.div 
+        initial={{opacity:0 , y:-40}}
+        animate={{opacity:1 , y:0}}
+        transition={{duration: 0.3}}
+        className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm border border-gray-200 px-8 py-4 flex justify-between items-center relative'>
+            <div className='flex items-center gap-3 cursor-pointer'>
+                <div className='bg-black text-white p-2 rounded-lg'>
+                    <BsRobot size={18}/>
+
+                </div>
