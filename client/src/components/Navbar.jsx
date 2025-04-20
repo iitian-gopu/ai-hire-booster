10faa4ebbb2f12ba17fc9cce8e634656cd64f3fd
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-04-20 18:16:51 +0530

    Iterate on Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -86,3 +86,14 @@ function Navbar() {
                         <div className='absolute right-0 mt-3 w-48 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50'>
                             <p className='text-md text-blue-500 font-medium mb-1'>{userData?.name}</p>
 
+                            <button onClick={()=>navigate("/history")} className='w-full text-left text-sm py-2 hover:text-black text-gray-600'>InterView History</button>
+                            <button onClick={handleLogout} 
+                            className='w-full text-left text-sm py-2 flex items-center gap-2 text-red-500'>
+                                <HiOutlineLogout size={16}/>
+                                Logout</button>
+                        </div>
+                    )}
+                </div>
+
+            </div>
+
