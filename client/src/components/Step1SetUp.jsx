a31541b28357c246c6d079fc00f58b02ce282f0c
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-04-21 16:24:39 +0530

    Set up Step1SetUp component

---
+++ client/src/components/Step1SetUp.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { motion } from "motion/react"
+import {
+    FaUserTie,
+    FaBriefcase,
+    FaFileUpload,
+    FaMicrophoneAlt,
+    FaChartLine,
+} from "react-icons/fa";
+import { useState } from 'react';
+import axios from "axios"
+import { ServerUrl } from '../App';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
+function Step1SetUp({ onStart }) {
+    const {userData}= useSelector((state)=>state.user)
+    const dispatch = useDispatch()
+    const [role, setRole] = useState("");
+    const [experience, setExperience] = useState("");
+    const [mode, setMode] = useState("Technical");
+    const [resumeFile, setResumeFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [projects, setProjects] = useState([]);
+    const [skills, setSkills] = useState([]);
+    const [resumeText, setResumeText] = useState("");
+    const [analysisDone, setAnalysisDone] = useState(false);
+    const [analyzing, setAnalyzing] = useState(false);
+
