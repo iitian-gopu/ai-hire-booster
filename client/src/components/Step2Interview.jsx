124dc1c2a235efe280c92426906f9cdc44784c0f
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-03 16:26:57 +0530

    Flesh out Step2Interview component

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -317,3 +317,48 @@ setIsSubmitting(false)
         recognitionRef.current.abort();
       }
 
+      window.speechSynthesis.cancel();
+    };
+  }, []);
+
+
+
+
+
+
+
+  return (
+    <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
+      <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden'>
+
+        {/* video section */}
+        <div className='w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200'>
+          <div className='w-full max-w-md rounded-2xl overflow-hidden shadow-xl'>
+            <video
+              src={videoSource}
+              key={videoSource}
+              ref={videoRef}
+              muted
+              playsInline
+              preload="auto"
+              className="w-full h-auto object-cover"
+            />
+          </div>
+
+          {/* subtitle */}
+          {subtitle && (
+            <div className='w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm'>
+              <p className='text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed'>{subtitle}</p>
+            </div>
+          )}
+
+
+          {/* timer Area */}
+          <div className='w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5'>
+            <div className='flex justify-between items-center'>
+              <span className='text-sm text-gray-500'>
+                Interview Status
+              </span>
+              {isAIPlaying && <span className='text-sm font-semibold text-emerald-600'>
+                {isAIPlaying ? "AI Speaking" : ""}
+              </span>}
