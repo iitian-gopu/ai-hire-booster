2035599a03a39258bff1ff4dd7fdfe47ab952dec
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-06 21:35:23 +0530

    Set up Step3Report component

---
+++ client/src/components/Step3Report.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { motion } from "motion/react"
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import jsPDF from "jspdf"
+import autoTable from "jspdf-autotable"
+
+function Step3Report({ report }) {
+  if (!report) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500 text-lg">Loading Report...</p>
+      </div>
+    );
+  }
+  const navigate = useNavigate()
+  const {
+    finalScore = 0,
+    confidence = 0,
+    communication = 0,
+    correctness = 0,
+    questionWiseScore = [],
+  } = report;
+
+  const questionScoreData = questionWiseScore.map((score, index) => ({
+    name: `Q${index + 1}`,
+    score: score.score || 0
+  }))
+
+  const skills = [
+    { label: "Confidence", value: confidence },
+    { label: "Communication", value: communication },
+    { label: "Correctness", value: correctness },
+  ];
