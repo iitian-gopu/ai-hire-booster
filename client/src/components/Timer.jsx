59eb1b34bc89c4e0d7f2ee2bf68da52714511ce9
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-12 18:04:38 +0530

    Set up Timer component

---
+++ client/src/components/Timer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+function Timer({ timeLeft, totalTime }) {
+    const percentage = (timeLeft/totalTime)*100
+  return (
+    <div className='w-20 h-20'>
+        <CircularProgressbar
+        value={percentage}
+        text={`${timeLeft}s`}
+        styles={buildStyles({
+          textSize: "28px",
