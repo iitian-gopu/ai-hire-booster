e8c89c010ee31fe27f595e5642a4e6798b0cecfc
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-15 16:23:45 +0530

    Iterate on Auth page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -9,3 +9,14 @@ import axios from 'axios';
 import { ServerUrl } from '../App';
 import { useDispatch } from 'react-redux';
 import { setUserData } from '../redux/userSlice';
+function Auth({isModel = false}) {
+    const dispatch = useDispatch()
+
+    const handleGoogleAuth = async () => {
+        try {
+            const response = await signInWithPopup(auth,provider)
+            let User = response.user
+            let name = User.displayName
+            let email = User.email
+            const result = await axios.post(ServerUrl + "/api/auth/google" , {name , email} , {withCredentials:true})
+            dispatch(setUserData(result.data))
