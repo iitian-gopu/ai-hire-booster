88da7cf4c7acf7a235e7bfaa5ce7df9877ff7c2f
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-15 14:43:37 +0530

    Scaffold Auth page

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BsRobot } from "react-icons/bs";
+import { IoSparkles } from "react-icons/io5";
+import { motion } from "motion/react"
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../utils/firebase';
+import axios from 'axios';
+import { ServerUrl } from '../App';
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
