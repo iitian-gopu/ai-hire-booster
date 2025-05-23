b68b5899c061a2358919121f5fb5ae43a1a6eecb
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-23 17:40:54 +0530

    Update Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,3 +30,34 @@ function Home() {
   const navigate = useNavigate()
   return (
     <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
+      <Navbar />
+
+      <div className='flex-1 px-6 py-20'>
+        <div className='max-w-6xl mx-auto'>
+
+          <div className='flex justify-center mb-6'>
+            <div className='bg-gray-100 text-gray-600 text-sm px-4 py-2 rounded-full flex items-center gap-2'>
+              <HiSparkles size={16} className="bg-green-50 text-green-600" />
+              AI Powered Smart Interview Platform
+            </div>
+
+
+          </div>
+          <div className='text-center mb-28'>
+            <motion.h1
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+              className='text-4xl md:text-6xl font-semibold leading-tight max-w-4xl mx-auto'>
+              Practice Interviews with
+              <span className='relative inline-block'>
+                <span className='bg-green-100 text-green-600 px-5 py-1 rounded-full'>
+                  AI Intelligence
+
+                </span>
+              </span>
+
+
+
+            </motion.h1>
+
