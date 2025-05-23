1a3e97f05f579b0d3758e7c6fb401916992e75c5
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-23 16:36:41 +0530

    Set up Home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { useSelector } from 'react-redux'
+import { motion } from "motion/react";
+import {
+  BsRobot,
+  BsMic,
+  BsClock,
+  BsBarChart,
+  BsFileEarmarkText
+} from "react-icons/bs";
+import { HiSparkles } from "react-icons/hi";
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import AuthModel from '../components/AuthModel';
+import hrImg from "../assets/HR.png";
+import techImg from "../assets/tech.png";
+import confidenceImg from "../assets/confi.png";
+import creditImg from "../assets/credit.png";
+import evalImg from "../assets/ai-ans.png";
+import resumeImg from "../assets/resume.png";
+import pdfImg from "../assets/pdf.png";
+import analyticsImg from "../assets/history.png";
+import Footer from '../components/Footer';
+
+
+function Home() {
+  const { userData } = useSelector((state) => state.user)
+  const [showAuth, setShowAuth] = useState(false);
+  const navigate = useNavigate()
+  return (
+    <div className='min-h-screen bg-[#f3f3f3] flex flex-col'>
