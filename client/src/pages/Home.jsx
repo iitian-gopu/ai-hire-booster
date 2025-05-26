c9b4a6b8438125a6673d0636c44c1c4e1ace6526
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-26 20:58:33 +0530

    Refine Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -156,3 +156,34 @@ function Home() {
           </div>
 
 
+          <div className='mb-32'>
+            <motion.h2
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+              className='text-4xl font-semibold text-center mb-16'>
+              Advanced AI{" "}
+              <span className="text-green-600">Capabilities</span>
+
+            </motion.h2>
+
+            <div className='grid md:grid-cols-2 gap-10'>
+              {
+                [
+                  {
+                    image: evalImg,
+                    icon: <BsBarChart size={20} />,
+                    title: "AI Answer Evaluation",
+                    desc: "Scores communication, technical accuracy and confidence."
+                  },
+                  {
+                    image: resumeImg,
+                    icon: <BsFileEarmarkText size={20} />,
+                    title: "Resume Based Interview",
+                    desc: "Project-specific questions based on uploaded resume."
+                  },
+                  {
+                    image: pdfImg,
+                    icon: <BsFileEarmarkText size={20} />,
+                    title: "Downloadable PDF Report",
+                    desc: "Detailed strengths, weaknesses and improvement insights."
