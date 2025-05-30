bc94aa9902e96d52f3d4ec5c1e98da8dc0833c88
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-05-30 16:32:24 +0530

    Scaffold InterviewHistory page

---
+++ client/src/pages/InterviewHistory.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from "axios"
+import { ServerUrl } from '../App'
+import { FaArrowLeft } from 'react-icons/fa'
+function InterviewHistory() {
+    const [interviews, setInterviews] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const getMyInterviews = async () => {
+            try {
