31a94ff0c0ef6dd70c8cee4a3a3642aaf555b906
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-06-10 20:46:26 +0530

    Iterate on InterviewReport page

--- client/src/pages/InterviewReport.jsx
+++ client/src/pages/InterviewReport.jsx
@@ -18,3 +18,13 @@ function InterviewReport() {
         console.log(error)
       }
     }
+
+    fetchReport()
+  },[])
+
+
+    if (!report) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-gray-500 text-lg">
+          Loading Report...
