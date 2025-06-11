0b18916895b2f6d160cbe87d04e1566140e7aa59
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-06-11 16:32:26 +0530

    Finalize InterviewReport page

--- client/src/pages/InterviewReport.jsx
+++ client/src/pages/InterviewReport.jsx
@@ -28,3 +28,12 @@ function InterviewReport() {
       <div className="min-h-screen flex items-center justify-center">
         <p className="text-gray-500 text-lg">
           Loading Report...
+        </p>
+      </div>
+    );
+  }
+
+  return <Step3Report report={report}/>
+}
+
+export default InterviewReport
