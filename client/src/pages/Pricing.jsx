4dcb78aeb4774412caa604bd5eda1661e64dbf39
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-06-11 18:12:31 +0530

    Scaffold Pricing page

---
+++ client/src/pages/Pricing.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { FaArrowLeft, FaCheckCircle } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+import { motion } from "motion/react";
+import axios from 'axios';
+import { ServerUrl } from '../App';
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
+function Pricing() {
+  const navigate = useNavigate()
+  const [selectedPlan, setSelectedPlan] = useState("free");
+  const [loadingPlan, setLoadingPlan] = useState(null);
+  const dispatch = useDispatch()
+
+  const plans = [
+    {
+      id: "free",
+      name: "Free",
+      price: "₹0",
+      credits: 100,
+      description: "Perfect for beginners starting interview preparation.",
+      features: [
+        "100 AI Interview Credits",
